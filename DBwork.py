d23911a133328d435102f1ca57b710580c18d611
mobulture <31808207+mobulture@users.noreply.github.com>
2021-05-09 12:38:11 -0400

    Added new credentials for readonly role

--- DBwork.py
+++ DBwork.py
@@ -2,36 +2,34 @@ import csv
 import psycopg2
 import sys
 import os
-'''
+
 ENDPOINT="nieloa.c7jvzdz9a72c.us-east-1.rds.amazonaws.com"
 PORT="5432"
-USR="master"
+USR = 'myuser'
 REGION="us-east-1f"
 DBNAME="myDatabase"
-token = 'masterguy'
-'''
-ENDPOINT = 'localhost'
-PORT ="5432"
-USER = "postgres"
-DBNAME = 'postgres'
-token = 'masterguy'
+token = 'password'
+
+
+#psql "host=nieloa.c7jvzdz9a72c.us-east-1.rds.amazonaws.com port=5432 dbname=myDatabase user=master password=masterguy"
+
+#slmode=verify-full sslrootcert=certificateFile 
+
 conn = None
 try:
-    conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=token)
+    conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USR, password=token)
     conn.autocommit= True
     cur = conn.cursor()
 
 except Exception as e:
     print("Connected failed due to {}".format(e))   
                  
-print(conn)
 #Use this for inserting or making changes to values
 def dbInsert(query):
     cursor = conn.cursor()
     try:
         cursor.execute(query)
         conn.commit()
-        print("TRUE")
     except Exception as e:
         print("QUERY error")
 #Use this for querying results
